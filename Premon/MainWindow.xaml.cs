2c4131d22468919480927fb5002f158ac0fe8f65
Timothee Kiehl <Timothee.Kiehl@etu.univ-smb.fr>
2024-12-10 16:06:24 +0100

    mouvement + taille carte

--- Premon/MainWindow.xaml.cs
+++ Premon/MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Premon
 {
@@ -16,9 +17,50 @@ namespace Premon
     /// </summary>
     public partial class MainWindow : Window
     {
+        private static readonly int PAS_DEPLACEMENT = 50;
+        private static DispatcherTimer intervalleDeplacement;
+        private static readonly int INTERVALLE_DEPLACEMENT = 500;
+        private static double gauche;
+        private static double haut;
         public MainWindow()
         {
             InitializeComponent();
+            InitIntervalleDeplacement();
+        }
+
+        private void fenetre_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.Left:
+                    gauche -= PAS_DEPLACEMENT;
+                    break;
+                case Key.Right:
+                    gauche += PAS_DEPLACEMENT;
+                    break;
+                case Key.Up:
+                    haut -= PAS_DEPLACEMENT;
+                    break;
+                case Key.Down:
+                    haut += PAS_DEPLACEMENT;
+                    break;
+            }
+        }
+
+        private void InitIntervalleDeplacement()
+        {
+            gauche = Canvas.GetLeft(perso);
+            haut = Canvas.GetTop(perso);
+            intervalleDeplacement = new DispatcherTimer();
+            intervalleDeplacement.Interval = TimeSpan.FromMilliseconds(INTERVALLE_DEPLACEMENT);
+            intervalleDeplacement.Tick += DeplacerVers;
+            intervalleDeplacement.Start();
+        }
+
+        private void DeplacerVers(object? sender, EventArgs e)
+        {
+            Canvas.SetLeft(perso, gauche);
+            Canvas.SetTop(perso, haut);
         }
     }
 }
