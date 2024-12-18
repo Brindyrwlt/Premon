a46a8d91113625e557bf302080d5e4c7c3d0afc0
Brindy <brindipote@gmail.com>
2024-12-18 12:29:55 +0100

    Commentaires de EcranAccueil.cs

--- Premon/EcranAccueil.xaml.cs
+++ Premon/EcranAccueil.xaml.cs
@@ -15,23 +15,27 @@ namespace Premon
             InitializeComponent();
         }
 
+        // Charger la partie est l'action par défaut
         private void BoutonChargerPartie_Click(object sender, RoutedEventArgs e)
         {
             DialogResult = true;
         }
 
+        // Suppression de la sauvegarde en cas de nouvelle partie
         private void BoutonNouvellePartie_Click(object sender, RoutedEventArgs e)
         {
             Inventaire.SuppressionSauvegarde();
             DialogResult = true;
         }
 
+        // Accéder aux paramètres
         private void BoutonParametres_Click(object sender, RoutedEventArgs e)
         {
             EcranParametres ecranParametres = new();
             ecranParametres.ShowDialog();
         }
 
+        // Quitter le jeu
         private void BoutonQuitter_Click(object sender, RoutedEventArgs e)
         {
 
